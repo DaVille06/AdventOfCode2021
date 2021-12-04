36aa146be756a514dcb0f7575e04d6f577bbbe73
Nick Roberts <roberts.nicholas88@gmail.com>
2021-12-03 23:06:14 -0500

    Completed solutions for Day1 and Day2.

---
+++ Day1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+
+            Part2();
+        }
+
+        static void Part1() 
+        {
+            var increases = 0;
+            // we don't want the first value to be counted
+            // we can assume the first depth won't be this large
+            var previousDepth = Int32.MaxValue;
+
+            foreach (var line in File.ReadLines("Inputs.txt")) 
+            {
+                var currentDepth = Int32.Parse(line);
+
+                if (currentDepth > previousDepth)
+                    increases++;
+
+                previousDepth = currentDepth;
+            }
+
+            Console.WriteLine($"Part 1 # of increases: {increases}");
+        }
+    
+        static void Part2() 
+        {
+            // max value indicates that we haven't filled the array yet
+            var array = new int[] {0, 0, 0, Int32.MaxValue};
+            var currentPosition = 0;
+            var increases = 0;
+            
+            foreach (var line in File.ReadLines("inputs.txt"))
+            {
+                var currentDepth = Int32.Parse(line);
+                array[currentPosition] = currentDepth;
+
+                if (array[3] != Int32.MaxValue)
+                {
+                    var latestValue = array[currentPosition];
+                    var oldValue = (currentPosition + 1 == 4) ? array[0] : array[currentPosition + 1];
+
+                    if (latestValue > oldValue) increases++;
+                }
+
+
+                currentPosition++;
+
+                currentPosition = (currentPosition > 3) ? 0 : currentPosition;
+            }
+
+            Console.WriteLine($"Part 2 # of increases: {increases}");
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var depth = 0;
+            var horizontal = 0;
+
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                var inputs = line.Split(' ');
+                // inputs[0] = direction
+                // inputs[1] = value;
+                var value = Int32.Parse(inputs[1]);
+
+                if (inputs[0].Equals("forward"))
+                    horizontal += value;
+                if (inputs[0].Equals("up"))
+                    depth -= value;
+                if (inputs[0].Equals("down"))
+                    depth += value;
+            }
+            
+            Console.WriteLine($"Part 1 Current Position:\thorizontal-{horizontal}\t\tdepth-{depth}");
+            Console.WriteLine($"Answer: {depth * horizontal}");
+        }
+
+        static void Part2()
+        {
+            var depth = 0;
+            var horizontal = 0;
+            var aim = 0;
+
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                var inputs = line.Split(' ');
+                // inputs[0] = direction
+                // inputs[1] = value;
+                var value = Int32.Parse(inputs[1]);
+
+                if (inputs[0].Equals("forward"))
+                {
+                    horizontal += value;
+                    depth += aim * value;
+                }
+                if (inputs[0].Equals("up"))
+                {
+                    aim -= value;
+                }
+                if (inputs[0].Equals("down"))
+                {
+                    aim += value;
+                }
+            }
+            
+            Console.WriteLine($"Part 1 Current Position:\thorizontal-{horizontal}\t\tdepth-{depth}\taim-{aim}");
+            Console.WriteLine($"Answer: {depth * horizontal}");
+        }
+    }
+}
+
+
+/*
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+*/
