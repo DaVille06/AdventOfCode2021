23485511924cd116fe28ec7d9a6894887cfd268b
Nick Roberts <roberts.nicholas88@gmail.com>
2021-12-07 21:50:59 -0500

    Have gotten all the data in structures.

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day4
 {
@@ -6,7 +8,48 @@ namespace Day4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> calledNumbers = null;
+            var grids = new List<Dictionary<string, Coordinate>>();
+            var currentGrid = -1;
+            var currentRow = 0;
+
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                if (calledNumbers is null)
+                {
+                    calledNumbers = line.Split(',').ToList();
+                }
+                else if (line is "")
+                {
+                    grids.Add(new Dictionary<string, Coordinate>());
+                    currentGrid++;
+                    currentRow = 0;
+                    continue;
+                }
+                else
+                {
+                    var gridData = line.Split(' ').ToList();
+                    gridData.RemoveAll(x => x == "");
+
+                    for (int i = 0; i < gridData.Count(); i++)
+                    {
+                        grids[currentGrid].Add(gridData[i], new Coordinate(currentRow, i));
+                    }
+                    currentRow++;
+                }
+            }
         }
+    }
+
+    public class Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            XValue = x;
+            YValue = y;
+        }
+
+        public int XValue { get; set; }
+        public int YValue { get; set; }
     }
 }
