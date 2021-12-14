efb2fa0a39df5104d4af91d9e205cdfdf05c7179
Nick Roberts <roberts.nicholas88@gmail.com>
2021-12-13 23:19:37 -0500

    Solved Day7 Part2 successfully.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -8,6 +8,12 @@ namespace Day7
     {
         static void Main(string[] args)
         {
+            Part1();
+            Part2();
+        }
+
+        public static void Part1()
+        {
             var fuelSpent = 0;
             // there's only one line
             foreach (var line in File.ReadLines("Inputs.txt"))
@@ -27,5 +33,39 @@ namespace Day7
 
             Console.WriteLine($"Part 1 Answer: {fuelSpent}");
         }
+
+        public static void Part2()
+        {
+            var fuelSpent = 0;
+            // there's only one line
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                var inputs = line.Split(',').Select(int.Parse).ToList();
+                var sum = 0;
+                foreach (var input in inputs)
+                {
+                    sum += input;
+                }
+
+                // only want to consider 1 decimal place here
+                var position = (int)Math.Round(((double)sum / inputs.Count()), 1);
+
+                Console.WriteLine($"Chosen Position: {position}");
+
+                foreach (var input in inputs)
+                {
+                    var difference = Math.Abs(input - position);
+                    for (int i = 1; i <= difference; i++)
+                    {
+                        fuelSpent += i;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Part 2 Answer: {fuelSpent}");
+        }
     }
 }
+
+
+// 100148861 - TOO HIGH
