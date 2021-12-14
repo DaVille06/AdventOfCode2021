ac2c86dd48dc5bb58e26c22711c405e3fa67db5b
Nick Roberts <roberts.nicholas88@gmail.com>
2021-12-13 22:51:01 -0500

    Day7 Part 1 answered successfully.

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Day7
 {
@@ -6,7 +8,24 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fuelSpent = 0;
+            // there's only one line
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                var inputs = line.Split(',').Select(int.Parse).ToList();
+                inputs.Sort();
+                var position = inputs.Count() / 2;
+                Console.WriteLine($"Chosen Position: {position}");
+                Console.WriteLine($"Value at the position: {inputs[position]}");
+
+                // each Math.Abs(input - input[position])
+                foreach (var input in inputs)
+                {
+                    fuelSpent += Math.Abs(input - inputs[position]);
+                }
+            }
+
+            Console.WriteLine($"Part 1 Answer: {fuelSpent}");
         }
     }
 }
