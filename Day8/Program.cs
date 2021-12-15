a0ee644f1530077fa01dcc94ae3a13491a195420
Nick Roberts <roberts.nicholas88@gmail.com>
2021-12-14 21:32:02 -0500

    Solved Day8 Part1 Successfully.

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Day8
 {
@@ -6,7 +8,35 @@ namespace Day8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Part1();
+        }
+
+        public static void Part1()
+        {
+            var count = 0;
+            // we are looking for 1 4 7 8
+            // this means things after the | which have either
+            // 2 3 4 or 7 letters in the count
+            foreach (var line in File.ReadLines("Inputs.txt"))
+            {
+                var delimitedSplit = line.Split('|');
+                var inputs = delimitedSplit[1].Split(" ").ToList();
+                inputs.RemoveAll(x => x == "");
+
+                // go through each input
+                // check for 2 3 4 or 7 count
+                foreach (var input in inputs)
+                {
+                    if (input.Count() == 2 ||
+                        input.Count() == 3 ||
+                        input.Count() == 4 ||
+                        input.Count() == 7)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Part1 Answer: {count}");
         }
     }
 }
